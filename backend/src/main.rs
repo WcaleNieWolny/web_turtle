84c1d02f2c69cf6545ffeef0cbf86bc47ac5fd2a
WcaleNieWolny <isupermichael007@gmail.com>
2023-04-19 20:02:14 +0200

    Split turtle command endpoint into individual entities

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,62 +1,13 @@
-use std::{net::SocketAddr, sync::Arc, collections::HashMap, time::Duration};
+mod turtle;
 
-use axum::{Router, extract::{WebSocketUpgrade, ConnectInfo, ws::{WebSocket, Message}, State, RawBody}, response::{IntoResponse, Response}, routing::{get, post, put}, http::StatusCode, body::Full, Json};
-use serde::Serialize;
-use thiserror::Error;
-use tokio::{sync::{Mutex, oneshot, mpsc}, time::timeout};
+use std::{net::SocketAddr, sync::Arc, collections::HashMap};
+use axum::{Router, extract::{WebSocketUpgrade, ConnectInfo, ws::{WebSocket, Message}, State, Path}, response::IntoResponse, routing::{get, put}, http::StatusCode, Json};
+use tokio::sync::{Mutex, mpsc};
 use tower_http::trace::{TraceLayer, DefaultMakeSpan};
 use tracing::error;
 use tracing_subscriber::{prelude::__tracing_subscriber_SubscriberExt, util::SubscriberInitExt};
+use turtle::{Turtle, TurtleRequestError, TurtleAsyncRequest};
 use uuid::Uuid;
-
-#[derive(Error, Debug)]
-pub enum TurtleRequestError {
-    #[error("Invalid WebSocket client response")] 
-    InvalidResponse,
-    #[error("Data send error")]
-    DataSendError(#[from] axum::Error),
-    #[error("Recive msg from WebSocket error")]
-    RecvMsgError,
-    #[error("WebSocket closed")]
-    WsClosed,
-    #[error("Timed out")]
-    TimeOut,
-    #[error("Cannot send request")]
-    RequestSendError,
-    #[error("Response recv error")]
-    ResponseRecvError
-}
-
-struct TurtleAsyncRequest {
-    request: String,
-    response: oneshot::Sender<Result<String, TurtleRequestError>>
-}
-
-#[derive(Debug)]
-struct Turtle {
-    request_queue: mpsc::Sender<TurtleAsyncRequest>
-}
-
-impl Turtle {
-    async fn command(&mut self, command: &String) -> Result<String, TurtleRequestError> {
-        let (tx, rx) = oneshot::channel::<Result<String, TurtleRequestError>>();
-
-        let request = TurtleAsyncRequest {
-            request: command.clone(),
-            response: tx,
-        };
-
-        match timeout(Duration::from_secs(5), self.request_queue.send(request)).await {
-            Ok(val) => val.or(Err(TurtleRequestError::RequestSendError))?,
-            Err(_) => return Err(TurtleRequestError::TimeOut),
-        }
-
-        match timeout(Duration::from_secs(10), rx).await {
-            Ok(val) => return val.or(Err(TurtleRequestError::ResponseRecvError))?,
-            Err(_) => return Err(TurtleRequestError::TimeOut),
-        }
-    }
-}
 
 #[derive(Clone, Default)]
 struct TurtlesState {
@@ -77,7 +28,7 @@ async fn main() {
     // build our application with some routes
     let app = Router::new()
         .route("/turtle/", get(ws_handler))
-        .route("/turtle/command/", put(command_turtle))
+        .route("/turtle/:id/command/", put(command_turtle))
         .route("/turtle/list/", get(list_turtles))
         // logging so we can see whats going on
         .layer(
@@ -108,19 +59,19 @@ async fn ws_handler(
 
 async fn command_turtle(
     State(turtles): State<TurtlesState>,
+    Path(uuid): Path<String>,
     command: String 
 ) -> Result<impl IntoResponse, (StatusCode, impl IntoResponse)> {
 
     let mut guard = turtles.turtles.lock().await;
 
-    for (_, v) in guard.iter_mut() {
-        match v.command(&command).await {
-            Ok(val) => return Ok(val),
-            Err(error) => return Err((StatusCode::INTERNAL_SERVER_ERROR, error.to_string()))
-        }
-    }
+    let uuid = Uuid::parse_str(&uuid).or(Err((StatusCode::BAD_REQUEST, StatusCode::BAD_REQUEST.to_string())))?;
+    let turtle = match guard.get_mut(&uuid) {
+        Some(v) => v,
+        None => return Err((StatusCode::NOT_FOUND, StatusCode::NOT_FOUND.to_string())) 
+    };
 
-    return Err((StatusCode::NOT_FOUND, StatusCode::NOT_FOUND.to_string()));
+    return turtle.command(&command).await.map_err(|err| (StatusCode::INTERNAL_SERVER_ERROR, err.to_string()));
 }
 
 async fn list_turtles(
@@ -132,7 +83,7 @@ async fn list_turtles(
         .collect());
 }
 
-async fn handle_socket(mut socket: WebSocket, addr: SocketAddr, turtles: TurtlesState)  {
+async fn handle_socket(mut socket: WebSocket, _addr: SocketAddr, turtles: TurtlesState)  {
 
     let (tx, mut rx) = mpsc::channel::<TurtleAsyncRequest>(64);
     let uuid = Uuid::new_v4();
---
+++ backend/src/turtle.rs
@@ -0,0 +1,51 @@
+use std::time::Duration;
+
+use thiserror::Error;
+use tokio::{sync::{oneshot, mpsc}, time::timeout};
+
+#[derive(Error, Debug)]
+pub enum TurtleRequestError {
+    #[error("Invalid WebSocket client response")] 
+    InvalidResponse,
+    #[error("Data send error")]
+    DataSendError(#[from] axum::Error),
+    #[error("WebSocket closed")]
+    WsClosed,
+    #[error("Timed out")]
+    TimeOut,
+    #[error("Cannot send request")]
+    RequestSendError,
+    #[error("Response recv error")]
+    ResponseRecvError
+}
+
+pub struct TurtleAsyncRequest {
+    pub request: String,
+    pub response: oneshot::Sender<Result<String, TurtleRequestError>>
+}
+
+#[derive(Debug)]
+pub struct Turtle {
+    pub request_queue: mpsc::Sender<TurtleAsyncRequest>
+}
+
+impl Turtle {
+    pub async fn command(&mut self, command: &String) -> Result<String, TurtleRequestError> {
+        let (tx, rx) = oneshot::channel::<Result<String, TurtleRequestError>>();
+
+        let request = TurtleAsyncRequest {
+            request: command.clone(),
+            response: tx,
+        };
+
+        match timeout(Duration::from_secs(5), self.request_queue.send(request)).await {
+            Ok(val) => val.or(Err(TurtleRequestError::RequestSendError))?,
+            Err(_) => return Err(TurtleRequestError::TimeOut),
+        }
+
+        match timeout(Duration::from_secs(10), rx).await {
+            Ok(val) => return val.or(Err(TurtleRequestError::ResponseRecvError))?,
+            Err(_) => return Err(TurtleRequestError::TimeOut),
+        }
+    }
+}
