5fcc83f3a14335f38f57af2a5b723e5a994963e3
WcaleNieWolny <isupermichael007@gmail.com>
2023-05-03 17:09:59 +0200

    Bevy migration part 2

--- bevy_frontend/src/main.rs
+++ bevy_frontend/src/main.rs
@@ -2,30 +2,40 @@ use std::f32::consts::TAU;
 
 use bevy::prelude::*;
 use bevy_panorbit_camera::{PanOrbitCamera, PanOrbitCameraPlugin};
+use js_sys::JsString;
+use wasm_bindgen::prelude::*;
+use web_sys::window;
+
+#[wasm_bindgen]
+pub fn shiet() -> u32 {
+   99 
+}
 
 fn main() {
     // When building for WASM, print panics to the browser console
     #[cfg(target_arch = "wasm32")]
     {
+        use log::Level;
         console_error_panic_hook::set_once();
+        console_log::init_with_level(Level::Warn).unwrap();
     }
     
-    App::new()
-        .add_plugins(
-            DefaultPlugins.build()
-                .set(WindowPlugin {
-                    primary_window: Some(Window {
-                        fit_canvas_to_parent: true,
-                        canvas: Some(".game_canvas".to_string()), 
-                        ..default()
-                    }),
+App::new()
+    .add_plugins(
+        DefaultPlugins.build()
+            .set(WindowPlugin {
+                primary_window: Some(Window {
+                    fit_canvas_to_parent: true,
+                    canvas: Some(".game_canvas".to_string()), 
                     ..default()
-                })
-        )
-        .insert_resource(Msaa::Sample4)
-        .add_plugin(PanOrbitCameraPlugin)
-        .add_startup_system(setup)
-        .run();
+                }),
+                ..default()
+            })
+    )
+    .insert_resource(Msaa::Sample4)
+    .add_plugin(PanOrbitCameraPlugin)
+    .add_startup_system(setup)
+    .run();
 }
 
 //https://bevyengine.org/examples/3d/3d-scene/
@@ -35,6 +45,11 @@ fn setup(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
+    //Export to JS
+    let window: JsValue = window().unwrap().into(); 
+    let shiet_str: JsValue = JsString::from("aa").into();
+    
+
     // plane
     commands.spawn(PbrBundle {
         mesh: meshes.add(shape::Plane::from_size(5.0).into()),
@@ -67,6 +82,6 @@ fn setup(
     });
     // background
     commands.insert_resource(ClearColor(Color::hex("5b7cb6").unwrap()));
-
-    println!("TESAT!");
 }
+
+
