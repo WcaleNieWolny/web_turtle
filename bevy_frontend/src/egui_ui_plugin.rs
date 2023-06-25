3508acf7327dd7519185c6ff5acb572854121e8d
WcaleNieWolny <isupermichael007@gmail.com>
2023-06-25 12:36:33 +0200

    Add refresh button into egui ui

--- bevy_frontend/src/egui_ui_plugin.rs
+++ bevy_frontend/src/egui_ui_plugin.rs
@@ -2,15 +2,26 @@ use std::sync::{Arc, RwLock};
 
 use bevy::prelude::*;
 use bevy_egui::{EguiContexts, egui::{self, Align2, Color32, Frame, Layout, Align, Ui, Margin, style::WidgetVisuals, Stroke, RichText, FontId, FontFamily, FontDefinitions, FontData, Rounding}};
+use egui_extras::RetainedImage;
 
 use crate::MainTurtle;
 
 pub struct UiPlugin;
 
+#[derive(Resource, Deref)]
+struct RefreshButtonImg(RetainedImage);
+
 impl Plugin for UiPlugin {
     fn build(&self, app: &mut App) {
         app
             .insert_resource(MainTurtle(Arc::new(RwLock::new(None))))
+            .insert_resource(RefreshButtonImg(
+                egui_extras::RetainedImage::from_svg_bytes_with_size(
+                    "update-icon.svg",
+                    include_bytes!("../assets/update-icon.svg"),
+                    egui_extras::image::FitTo::Original
+                ).unwrap()
+            ))
             .add_startup_system(setup_font)
             .add_system(draw_egui_ui);
     }
@@ -30,6 +41,7 @@ fn setup_font(mut contexts: EguiContexts) {
 
 fn draw_egui_ui(
     mut contexts: EguiContexts,
+    image: Res<RefreshButtonImg>
 ) {
     egui::panel::TopBottomPanel::new(egui::panel::TopBottomSide::Top, "aaa")
         //.pivot(Align2::LEFT_TOP)
@@ -37,6 +49,12 @@ fn draw_egui_ui(
         //.title_bar(false)
         .frame(Frame {
             fill: Color32::from_rgb(41, 37, 36),
+            inner_margin: Margin {
+                left: 4.,
+                right: 8.,
+                top: 0.,
+                bottom: 0.,
+            },
             ..default()
         })
         .resizable(false)
@@ -46,6 +64,23 @@ fn draw_egui_ui(
             ui.with_layout(Layout::left_to_right(Align::Min), |ui| {
                 turtle_button(ui);
                 turtle_button(ui);
+
+                ui.with_layout(Layout::right_to_left(Align::Center), |ui| {
+                    let margin = egui::Frame::none()
+                        .fill(egui::Color32::from_rgb(6, 182, 212))
+                        .outer_margin(Margin::symmetric(0., 10.))
+                        //.outer_margin(Margin::same(4.))
+                        .inner_margin(Margin::same(1.5))
+                        .rounding(Rounding::same(9999.))
+                        .stroke(Stroke::new(8., Color32::from_rgb(6, 182, 212)));
+
+
+                    margin.show(ui, |ui| {
+                        //image.show_size(ui, [32., 32.].into()); 
+                        image.show_max_size(ui, [32., 32.].into());
+                    })
+                });
+
             });
         });
 }
